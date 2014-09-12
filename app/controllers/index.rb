8458142b82234619b497a1b314ce2af55ffda923
bobandraa <sandra.cohen.ux@gmail.com>
2014-09-11 21:23:11 -0700

    added email form partial

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,28 @@
+require 'pony'
+
 get '/' do
    erb :index
 end
 
+get '/contact' do
 
+  erb :"_contact_form"
+end
+
+post '/contact' do
+  Pony.mail :to => 'sandra.cohen.ux@gmail.com',
+            :subject => params[:email] + " would like to contact you!",
+            :from => params[:email],
+            :body => params[:content],
+            :via => :smtp,
+            :via_options => {
+              :address    => 'smtp.gmail.com',
+              :port       => '587',
+              :enable_starttls_auto => true,
+              :user_name  => 'devsandraa',
+              :password   => 'devbootcamp',
+              :authentication => :plain,
+              :domain     => "localhost.localdomain"
+            }
+  redirect '/'
+end
